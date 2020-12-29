b6c851906a3a4ad4961b54c1ee9f27c546215520
nicholasanthonys <anthony.nicholas84@gmail.com>
2020-12-29 23:00:12 +0700

    add vectorized technique

--- ex2/predict.m
+++ ex2/predict.m
@@ -15,15 +15,17 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-for i=1:m 
-    if sigmoid(X(i,:) * theta ) >= 0.5
-        p(i,1) = 1;
-    else
-        p(i,1) = 0;
-    end
-end
+% Loop technique example
+% for i=1:m 
+%     if sigmoid(X(i,:) * theta ) >= 0.5
+%         p(i,1) = 1;
+%     else
+%         p(i,1) = 0;
+%     end
+% end
 
-
+% Vectorized technique example
+p = sigmoid(X * theta) >= 0.5;
 
 
 
